9021ee17469e8d552630a14bf12ced74f5c2cbb5
Eduard Stefan <eduardstefan290@gmail.com>
2021-01-19 21:10:44 +0100

    commit for remote

--- src/core/Router.php
+++ src/core/Router.php
@@ -54,7 +54,7 @@ class Router
     $path = $this->request->getPath();
     $method = $this->request->method();
 
-    $params = $this->getRegPath();
+    $params = $this->resolveRoute();
     echo json_encode($params);
     exit();
     if ($params === false) {
@@ -75,7 +75,7 @@ class Router
     return call_user_func($callback, $this->request, $this->response, $params);
   }
 
-  private function getReqParams()
+  private function resolveRoute()
   {
     $pathMatches = true;
 
@@ -83,55 +83,31 @@ class Router
     $pathArr = explode('/', $this->request->getPath());
     array_shift($pathArr);
 
+
     foreach (array_keys($this->routes[$method]) as $registeredPath) {
+
       $pathMatches = true;
       $params = [];
       $registeredPathArr = explode('/', $registeredPath);
       array_shift($registeredPathArr);
-      foreach ($registeredPathArr as $pos => $registeredPathFragment) {
-        if (substr($registeredPathFragment, 0, 1) === ':') {
-          $params[substr($registeredPathFragment, 1)] = $pathArr[$pos];
-        } else if ($registeredPathFragment !== $pathArr[$pos]) {
-          $pathMatches = false;
-          break;
-        }
-      }
-    }
-    if ($pathMatches) {
-      return ['regPath' => $registeredPath, 'params' => $params];
-    }
 
-    // throw new NotFoundException();
-  }
-
-  private function getRegPath()
-  {
-    $pathMatches = true;
-
-    $method = $this->request->method();
-    $pathArr = explode('/', $this->request->getPath());
-    array_shift($pathArr);
-
-    foreach (array_keys($this->routes[$method]) as $registeredPath) {
-      $pathMatches = true;
-      $registeredPathArr = explode('/', $registeredPath);
-      echo $registeredPath;
-      echo ' ---- ';
-      echo $this->request->getPath();
-      array_shift($registeredPathArr);
-      foreach ($registeredPathArr as $pos => $registeredPathFragment) {
-        if (substr($registeredPathFragment, 0, 1) === ':') {
-          // $params[substr($registeredPathFragment, 1)] = $pathArr[$pos];
-        } else if ($registeredPathFragment !== $pathArr[$pos]) {
-          $pathMatches = false;
-          break;
+      if (count($registeredPathArr) !== count($pathArr)) {
+        $pathMatches = false;
+      } else {
+        foreach ($registeredPathArr as $pos => $registeredPathFragment) {
+          if (substr($registeredPathFragment, 0, 1) === ':') {
+            $params[substr($registeredPathFragment, 1)] = $pathArr[$pos];
+          } else if ($registeredPathFragment !== $pathArr[$pos]) {
+            $pathMatches = false;
+            break;
+          }
         }
       }
       if ($pathMatches !== false) {
-        return $registeredPath;
+        return ['regPath' => $registeredPath, 'params' => $params];
       }
     }
 
-    return false;
+    throw new NotFoundException();
   }
 }
